b829d8da0d6cf013112279849474260c06636cae
Kilian Kornel <kilianivan@gmail.com>
2023-03-02 22:58:05 +0100

    hazi01 teszt1

---
+++ HAZI/HAZI01/HAZI01.py
@@ -0,0 +1,143 @@
+# %%
+#Create a function that returns with a subsest of a list.
+#The subset's starting and ending indexes should be set as input parameters (the list aswell).
+#return type: list
+#function name must be: subset
+#input parameters: input_list,start_index,end_index
+
+# %%
+def subset(input_list,start_index,end_index):
+    for i in range(start_index,end_index):
+        mylist=[]
+        mylist.append=input_list[i]
+        return mylist
+
+
+
+# %%
+#Create a function that returns every nth element of a list.
+#return type: list
+#function name must be: every_nth
+#input parameters: input_list,step_size
+
+# %%
+def every_nth(input_list,step_size):
+    for i in range(input_list,len(input_list),step_size):
+        mylist=[]
+        mylist.append=input_list[i]
+        return mylist
+
+# %%
+#Create a function that can decide whether a list contains unique values or not
+#return type: bool
+#function name must be: unique
+#input parameters: input_list
+
+# %%
+def unique(input_list):
+    return len(set(input_list))==len(input_list)
+
+# %%
+#Create a function that can flatten a nested list ([[..],[..],..])
+#return type: list
+#fucntion name must be: flatten
+#input parameters: input_list
+
+# %%
+def flatten(input_list):
+    flat = [num for sublist in input_list for num in sublist]
+    return flat
+
+# %%
+#Create a function that concatenates n lists
+#return type: list
+#function name must be: merge_lists
+#input parameters: *args
+
+
+# %%
+def merge_lists(*args):
+    return [sum(l, []) for l in zip(*args)]
+
+# %%
+#Create a function that can reverse a list of tuples
+#example [(1,2),...] => [(2,1),...]
+#return type: list
+#fucntion name must be: reverse_tuples
+#input parameters: input_list
+
+# %%
+def reverse_tuples(input_list):
+    mylist= [t[::1] for t in input_list]
+    return mylist
+
+# %%
+#Create a function that removes duplicates from a list
+#return type: list
+#fucntion name must be: remove_tuplicates
+#input parameters: input_list
+
+# %%
+def remove_duplicates(input_list):
+    return list(set(input_list))
+
+
+# %%
+#Create a function that transposes a nested list (matrix)
+#return type: list
+#function name must be: transpose
+#input parameters: input_list
+
+# %%
+def transpose(input_list):
+    mylist= [[input_list[j][i] for j in range(len(input_list))] for i in range(len(input_list[0]))]
+    return mylist
+
+# %%
+#Create a function that can split a nested list into chunks
+#chunk size is given by parameter
+#return type: list
+#function name must be: split_into_chunks
+#input parameters: input_list,chunk_size
+
+# %%
+
+
+# %%
+#Create a function that can merge n dictionaries
+#return type: dictionary
+#function name must be: merge_dicts
+#input parameters: *dict
+
+# %%
+def merge_dicts(*dict):
+    mydict={}
+    for d in zip(*dict):
+        mydict.update(d)
+    return mydict
+
+# %%
+#Create a function that receives a list of integers and sort them by parity
+#and returns with a dictionary like this: {"even":[...],"odd":[...]}
+#return type: dict
+#function name must be: by_parity
+#input parameters: input_list
+
+# %%
+
+
+# %%
+#Create a function that receives a dictionary like this: {"some_key":[1,2,3,4],"another_key":[1,2,3,4],....}
+#and return a dictionary like this : {"some_key":mean_of_values,"another_key":mean_of_values,....}
+#in short calculates the mean of the values key wise
+#return type: dict
+#function name must be: mean_key_value
+#input parameters: input_dict
+
+# %%
+
+
+# %%
+#If all the functions are created convert this notebook into a .py file and push to your repo
+
+
