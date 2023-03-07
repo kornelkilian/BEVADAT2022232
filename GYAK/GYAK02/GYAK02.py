092468c583b6875dd5d6504e2e01637bec1b210a
Kilian Kornel <kilianivan@gmail.com>
2023-03-07 11:22:44 +0100

    GYAK02

---
+++ GYAK/GYAK02/GYAK02.py
@@ -0,0 +1,85 @@
+# %%
+import numpy as np
+
+# %%
+
+#FONTOS!!!
+
+# CSAK OTT LEHET HASZNÁLNI FOR LOOP-OT AHOL A FELADAT KÜLÖN KÉRI!
+
+# %%
+#Készíts egy függvényt ami létre hoz egy nullákkal teli numpy array-t.
+#Paraméterei: mérete (tupel-ként), default mérete pedig legyen egy (2,2)
+#Be: (2,2)
+#Ki: [[0,0],[0,0]]
+#create_array()
+
+# %%
+def create_array(size=(2,2)):
+    return np.zeros(size, dtype=int)
+
+# %%
+#Készíts egy függvényt ami a paraméterként kapott array-t főátlót feltölti egyesekkel
+#Be: [[1,2],[3,4]]
+#Ki: [[1,2],[3,1]]
+#set_one()
+
+# %%
+def set_one(array):
+   return np.fill_diagonal(array,1)
+
+# %%
+# Transzponáld a paraméterül kapott mártix-ot:
+# Be: [[1, 2], [3, 4]]
+# Ki: [[1, 2], [3, 4]]
+# do_transpose()
+
+# %%
+def do_transpose(m):
+    return np.transpose(m)
+
+# %%
+# Készíts egy olyan függvényt ami az array-ben lévő értékeket N tizenedjegyik kerekíti, alapértelmezetten 
+# Be: [0.1223, 0.1675], n = 2
+# Ki: [0.12, 0.17]
+# round_array()
+
+# %%
+def round_array(m,n=2):
+    return np.around(m,n)
+
+
+# %%
+# Készíts egy olyan függvényt, ami a bementként  0 és 1 ből álló tömben a 0 - False-ra az 1 True-ra cserélni
+# Be: [[1, 0, 0], [1, 1, 1],[0, 0, 0]]
+# Ki: [[ True False False], [ True  True  True], [False False False]]
+# bool_array()
+
+# %%
+def bool_array(array:np.array):
+    return array.astype(np.bool_)
+
+
+# %%
+# Készíts egy olyan függvényt, ami a bementként  0 és 1 ből álló tömben a 1 - False-ra az 0 True-ra cserélni
+# Be: [[1, 0, 0], [1, 1, 1],[0, 0, 0]]
+# Ki: [[ True False False], [ True  True  True], [False False False]]
+# invert_bool_array()
+
+# %%
+def bool_array(array:np.array):
+    return  array==0
+
+
+# %%
+# Készíts egy olyan függvényt ami a paraméterként kapott array-t kilapítja
+# Be: [[1,2], [3,4]]
+# Ki: [1,2,3,4]
+# flatten()
+
+# %%
+def flatten(array:np.array):
+    return array.flatten()
+
+
+
