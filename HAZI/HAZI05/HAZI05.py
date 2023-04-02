85581588b3b7f93bb12eeea74dce5901cb233076
Kilian Kornel <kilianivan@gmail.com>
2023-04-02 20:32:41 +0200

    Update HAZI05.py

--- HAZI/HAZI05/HAZI05.py
+++ HAZI/HAZI05/HAZI05.py
@@ -2,23 +2,20 @@ import pandas as pd
 from typing import Tuple
 from scipy.stats import mode
 from sklearn.metrics import confusion_matrix
+import numpy as np
 
 class KNNClassifier:
     def init(self, k: int, test_split_ratio: float):
         self.k = k
         self.test_split_ratio = test_split_ratio
 
-    def load_csv(self, csv_path: str) -> Tuple[pd.DataFrame, pd.DataFrame]:
-        dataset = pd.read_csv(csv_path, header=None)
+    @staticmethod
+    def load_csv(csv_path:str) ->Tuple[pd.DataFrame, pd.DataFrame]:
+        dataset = pd.read_csv(csv_path, delimiter=',')
         dataset = dataset.sample(frac=1, random_state=42).reset_index(drop=True)
-        x, y = dataset.iloc[:, :4], dataset.iloc[:, -1]
-        x = x.fillna(3.5)
-        indices = (x > 13.0) | (x < 0.0)
-        x = x[~indices.any(axis=1)]
-        y = y[~indices.any(axis=1)]
-        x_train, y_train, x_test, y_test = self.train_test_split(x, y)
-        return x_train, y_train, x_test, y_test
-
+        x,y = pd.DataFrame(dataset.iloc[:,:8]), pd.DataFrame(dataset.iloc[:,-1])
+        return x, y
+    
     def train_test_split(self, features: pd.DataFrame, labels: pd.DataFrame) -> None:
         test_size = int(len(features) * self.test_split_ratio)
         train_size = len(features) - test_size
@@ -26,9 +23,12 @@ class KNNClassifier:
         self.x_train, self.y_train = features[:train_size], labels[:train_size]
         self.x_test, self.y_test = features[train_size:train_size + test_size], labels[train_size:train_size + test_size]
 
-    def euclidean(self, element_of_x: pd.DataFrame) -> pd.DataFrame:
-        return pd.Series(((self.x_train - element_of_x) ** 2).sum(axis=1)).apply(pd.np.sqrt)
-
+    def euclidean(self, element_of_x:pd.DataFrame) -> pd.DataFrame:
+        points = self.x_train.reset_index(drop=True)
+        element_of_x = element_of_x.reindex(points.index).ffill()
+        distances = ((points - element_of_x) ** 2).sum(axis=1) ** 0.5
+        return pd.DataFrame(distances, columns=['distance'])
+    
     def predict(self, x_test: pd.DataFrame) -> None:
         labels_pred = []
         for i, x_test_element in x_test.iterrows():
@@ -42,6 +42,17 @@ class KNNClassifier:
     def accuracy(self) -> float:
         true_positive = (self.y_test == self.y_preds).sum()
         return true_positive / len(self.y_test) * 100
+
+    def best_k(self) -> float:
+        old_k = self.k
+        results = []
+        for k in range(1,21):
+            self.k = k
+            self.predict(self.x_test)
+            res = round(self.accuracy(), 2)
+            results.append((k, res))
+        self.k = old_k
+        return max(results, key=lambda x:x[1])
 
     def confusion_matrix(self) -> pd.DataFrame:
         return confusion_matrix(self.y_test, self.y_preds)
