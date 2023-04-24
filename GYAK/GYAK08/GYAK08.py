7d13ea7f99dbb8e270630e8e4c651a7004c6e7bc
Kilian Kornel <kilianivan@gmail.com>
2023-04-24 11:32:36 +0200

    Fixes to GYAK08

--- GYAK/GYAK08/GYAK08.py
+++ GYAK/GYAK08/GYAK08.py
@@ -2,5 +2,5 @@ from LinearRegressionSkeleton import LinearRegression
 
 model = LinearRegression()
 
-model.fit(model.X, model.y)
+model.fit(model.X_train, model.y_train)
 model.predict(model.X_test)
--- GYAK/GYAK08/LinearRegressionSkeleton.py
+++ GYAK/GYAK08/LinearRegressionSkeleton.py
@@ -24,19 +24,18 @@ class LinearRegression:
         
 
     def fit(self, X: np.array, y: np.array):
+            n = float(len(X))
+            self.losses = []
+            for i in range(self.epochs): 
+                y_pred = self.m*X + self.c 
+                residuals = y_pred - y
+                loss = np.sum(residuals ** 2)
+                self.losses.append(loss)
+                D_m = (-2/n) * sum(X * residuals)  
+                D_c = (-2/n) * sum(residuals)  
+                self.m = self.m + self.lr * D_m  
+                self.c = self.c + self.lr * D_c  
 
-       
-        self.losses = []
-        for i in range(self.epochs): 
-            y_pred = self.m*X + self.c  # The current predicted value of Y
-
-            residuals = y_pred - y
-            loss = np.sum(residuals ** 2)
-            self.losses.append(loss)
-            D_m = (-2/n) * sum(X * residuals)  # Derivative wrt m
-            D_c = (-2/n) * sum(residuals)  # Derivative wrt c
-            self.m = self.m + self.lr * D_m  # Update m
-            self.c = self.c + self.lr * D_c  # Update c
         
 
     def predict(self, X):
