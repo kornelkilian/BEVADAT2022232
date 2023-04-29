946a5bc356ffc173f116a3033c7824b200ed2005
Kilian Kornel <kilianivan@gmail.com>
2023-04-29 13:02:04 +0200

    ADDED HAZI09

---
+++ HAZI/HAZI09/HAZI09.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set()
+import numpy as np
+from sklearn.cluster import KMeans
+from sklearn.metrics import accuracy_score
+from scipy.stats import mode
+from sklearn.metrics import confusion_matrix
+from sklearn.datasets import load_digits as digits
+
+
+class KMeansOnDigits:
+    def __init__(self, n_clusters, random_state):
+        self.n_clusters = n_clusters
+        self.random_state = random_state
+        
+    def load_dataset(self):
+        self.digits = digits()
+        
+    def predict(self):
+        self.model = KMeans(n_clusters=self.n_clusters, random_state=self.random_state)
+        self.clusters = self.model.fit_predict(self.digits.data)
+        
+    def get_labels(self):
+        self.labels = np.zeros_like(self.clusters)
+        for i in range(self.n_clusters):
+            mask = (self.clusters == i)
+            self.labels[mask] = mode(self.digits.target[mask])[0]
+        
+    def calc_accuracy(self):
+        self.accuracy = accuracy_score(self.labels, self.clusters)
+        
+    def confusion_matrix(self):
+        self.mat = confusion_matrix(self.labels, self.clusters)
